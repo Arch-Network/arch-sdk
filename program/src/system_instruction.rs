3cb0f26ec1bf36649932fa82cb90089cf83f6ce5
GitHub Action <action@github.com>
2025-09-23 10:14:04 +0000

    Sync SDK, Program, Test SDK, Token, and Associated Token Account from main repository

--- program/src/system_instruction.rs
+++ program/src/system_instruction.rs
@@ -114,49 +114,126 @@ pub enum SystemInstruction {
         space: u64,
     },
 
-    /// Consumes a stored nonce, replacing it with a successor
+    /// Create a new account at an address derived from a base pubkey and a seed
     ///
     /// # Account references
-    ///   0. `[WRITE]` Nonce account
-    ///   1. `[]` RecentBlockhashes sysvar
-    ///   2. `[SIGNER]` Nonce authority
-    AdvanceNonceAccount,
+    ///   0. `[WRITE, SIGNER]` Funding account
+    ///   1. `[WRITE]` Created account
+    ///   2. `[SIGNER]` (optional) Base account; the account matching the base Pubkey below must be
+    ///      provided as a signer, but may be the same as the funding account
+    ///      and provided as account 0
+    CreateAccountWithSeed {
+        /// Base public key
+        base: Pubkey,
 
-    /// Withdraw funds from a nonce account
-    ///
-    /// # Account references
-    ///   0. `[WRITE]` Nonce account
-    ///   1. `[WRITE]` Recipient account
-    ///   2. `[]` RecentBlockhashes sysvar
-    ///   3. `[]` Rent sysvar
-    ///   4. `[SIGNER]` Nonce authority
-    ///
-    /// The `u64` parameter is the lamports to withdraw, which must leave the
-    /// account balance above the rent exempt reserve or at zero.
-    WithdrawNonceAccount(u64),
+        /// String of ASCII chars, no longer than `Pubkey::MAX_SEED_LEN`
+        seed: String,
 
-    /// Drive state of Uninitialized nonce account to Initialized, setting the nonce value
-    ///
-    /// # Account references
-    ///   0. `[WRITE]` Nonce account
-    ///   1. `[]` RecentBlockhashes sysvar
-    ///   2. `[]` Rent sysvar
-    ///
-    /// The `Pubkey` parameter specifies the entity authorized to execute nonce
-    /// instruction on the account
-    ///
-    /// No signatures are required to execute this instruction, enabling derived
-    /// nonce account addresses
-    InitializeNonceAccount(Pubkey),
+        /// Number of lamports to transfer to the new account
+        lamports: u64,
 
-    /// Change the entity authorized to execute nonce instructions on the account
+        /// Number of bytes of memory to allocate
+        space: u64,
+
+        /// Owner program account address
+        owner: Pubkey,
+    },
+
+    /// Allocate space for and assign an account at an address
+    /// derived from a base public key and a seed
     ///
     /// # Account references
-    ///   0. `[WRITE]` Nonce account
-    ///   1. `[SIGNER]` Nonce authority
+    ///   0. `[WRITE]` Allocated account
+    ///   1. `[SIGNER]` Base account
+    AllocateWithSeed {
+        /// Base public key
+        base: Pubkey,
+
+        /// String of ASCII chars, no longer than `pubkey::MAX_SEED_LEN`
+        seed: String,
+
+        /// Number of bytes of memory to allocate
+        space: u64,
+
+        /// Owner program account
+        owner: Pubkey,
+    },
+
+    /// Assign account to a program based on a seed
     ///
-    /// The `Pubkey` parameter identifies the entity to authorize
-    AuthorizeNonceAccount(Pubkey),
+    /// # Account references
+    ///   0. `[WRITE]` Assigned account
+    ///   1. `[SIGNER]` Base account
+    AssignWithSeed {
+        /// Base public key
+        base: Pubkey,
+
+        /// String of ASCII chars, no longer than `pubkey::MAX_SEED_LEN`
+        seed: String,
+
+        /// Owner program account
+        owner: Pubkey,
+    },
+
+    /// Transfer lamports from a derived address
+    ///
+    /// # Account references
+    ///   0. `[WRITE]` Funding account
+    ///   1. `[SIGNER]` Base for funding account
+    ///   2. `[WRITE]` Recipient account
+    TransferWithSeed {
+        /// Amount to transfer
+        lamports: u64,
+
+        /// Seed to use to derive the funding account address
+        from_seed: String,
+
+        /// Owner to use to derive the funding account address
+        from_owner: Pubkey,
+    },
+    // /// Consumes a stored nonce, replacing it with a successor
+    // ///
+    // /// # Account references
+    // ///   0. `[WRITE]` Nonce account
+    // ///   1. `[]` RecentBlockhashes sysvar
+    // ///   2. `[SIGNER]` Nonce authority
+    // AdvanceNonceAccount,
+
+    // /// Withdraw funds from a nonce account
+    // ///
+    // /// # Account references
+    // ///   0. `[WRITE]` Nonce account
+    // ///   1. `[WRITE]` Recipient account
+    // ///   2. `[]` RecentBlockhashes sysvar
+    // ///   3. `[]` Rent sysvar
+    // ///   4. `[SIGNER]` Nonce authority
+    // ///
+    // /// The `u64` parameter is the lamports to withdraw, which must leave the
+    // /// account balance above the rent exempt reserve or at zero.
+    // WithdrawNonceAccount(u64),
+
+    // /// Drive state of Uninitialized nonce account to Initialized, setting the nonce value
+    // ///
+    // /// # Account references
+    // ///   0. `[WRITE]` Nonce account
+    // ///   1. `[]` RecentBlockhashes sysvar
+    // ///   2. `[]` Rent sysvar
+    // ///
+    // /// The `Pubkey` parameter specifies the entity authorized to execute nonce
+    // /// instruction on the account
+    // ///
+    // /// No signatures are required to execute this instruction, enabling derived
+    // /// nonce account addresses
+    // InitializeNonceAccount(Pubkey),
+
+    // /// Change the entity authorized to execute nonce instructions on the account
+    // ///
+    // /// # Account references
+    // ///   0. `[WRITE]` Nonce account
+    // ///   1. `[SIGNER]` Nonce authority
+    // ///
+    // /// The `Pubkey` parameter identifies the entity to authorize
+    // AuthorizeNonceAccount(Pubkey),
 }
 
 /// Creates a new account instruction linked to a specific UTXO.
@@ -274,6 +351,104 @@ pub fn sign_input(index: u32, signer: &Pubkey) -> Instruction {
     Instruction::new_with_bincode(
         SYSTEM_PROGRAM_ID,
         &SystemInstruction::SignInput { index },
+        account_metas,
+    )
+}
+
+pub fn create_account_with_seed(
+    from_pubkey: &Pubkey,
+    to_pubkey: &Pubkey, // must match create_with_seed(base, seed, owner)
+    base: &Pubkey,
+    seed: &str,
+    lamports: u64,
+    space: u64,
+    owner: &Pubkey,
+) -> Instruction {
+    let mut account_metas = vec![
+        AccountMeta::new(*from_pubkey, true),
+        AccountMeta::new(*to_pubkey, false),
+    ];
+    if base != from_pubkey {
+        account_metas.push(AccountMeta::new_readonly(*base, true));
+    }
+
+    Instruction::new_with_bincode(
+        SYSTEM_PROGRAM_ID,
+        &SystemInstruction::CreateAccountWithSeed {
+            base: *base,
+            seed: seed.to_string(),
+            lamports,
+            space,
+            owner: *owner,
+        },
+        account_metas,
+    )
+}
+
+pub fn assign_with_seed(
+    address: &Pubkey, // must match create_with_seed(base, seed, owner)
+    base: &Pubkey,
+    seed: &str,
+    owner: &Pubkey,
+) -> Instruction {
+    let account_metas = vec![
+        AccountMeta::new(*address, false),
+        AccountMeta::new_readonly(*base, true),
+    ];
+    Instruction::new_with_bincode(
+        SYSTEM_PROGRAM_ID,
+        &SystemInstruction::AssignWithSeed {
+            base: *base,
+            seed: seed.to_string(),
+            owner: *owner,
+        },
+        account_metas,
+    )
+}
+
+pub fn transfer_with_seed(
+    from_pubkey: &Pubkey, // must match create_with_seed(base, seed, owner)
+    from_base: &Pubkey,
+    from_seed: String,
+    from_owner: &Pubkey,
+    to_pubkey: &Pubkey,
+    lamports: u64,
+) -> Instruction {
+    let account_metas = vec![
+        AccountMeta::new(*from_pubkey, false),
+        AccountMeta::new_readonly(*from_base, true),
+        AccountMeta::new(*to_pubkey, false),
+    ];
+    Instruction::new_with_bincode(
+        SYSTEM_PROGRAM_ID,
+        &SystemInstruction::TransferWithSeed {
+            lamports,
+            from_seed,
+            from_owner: *from_owner,
+        },
+        account_metas,
+    )
+}
+
+pub fn allocate_with_seed(
+    address: &Pubkey, // must match create_with_seed(base, seed, owner)
+    base: &Pubkey,
+    seed: &str,
+    space: u64,
+    owner: &Pubkey,
+) -> Instruction {
+    let account_metas = vec![
+        AccountMeta::new(*address, false),
+        AccountMeta::new_readonly(*base, true),
+    ];
+    Instruction::new_with_bincode(
+        SYSTEM_PROGRAM_ID,
+        &SystemInstruction::AllocateWithSeed {
+            base: *base,
+            seed: seed.to_string(),
+            space,
+            owner: *owner,
+        },
         account_metas,
     )
 }
