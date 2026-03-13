0f2ec2b10aa67d3f0c6b3bb8d68d3bf48ad805fb
GitHub Action <action@github.com>
2026-03-13 10:02:11 +0000

    Sync SDK, Program, Token, Associated Token Account, and Token Metadata from main repository

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -1,7 +1,6 @@
 /// This module defines the instruction data structure and related error types.
 /// Instructions are the fundamental unit of program execution in the Arch Network,
 /// containing the program to call, accounts to interact with, and instruction data.
-
 #[cfg(feature = "fuzzing")]
 use libfuzzer_sys::arbitrary;
 use thiserror::Error;
