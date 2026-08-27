14e5668632ca33f594c9711e338fca658fc90f12
GitHub Action <action@github.com>
2026-08-27 11:51:15 +0000

    Sync SDK, Program, Token, Associated Token Account, and Token Metadata from main repository

---
+++ target/debug/deps/apl_associated_token_account-94a76aaed234dc32.d
@@ -0,0 +1,6 @@
+/home/runner/work/arch-network/arch-network/sdk-repo/target/debug/deps/apl_associated_token_account-94a76aaed234dc32.d: associated-token-account/src/lib.rs associated-token-account/src/tools.rs
+
+/home/runner/work/arch-network/arch-network/sdk-repo/target/debug/deps/libapl_associated_token_account-94a76aaed234dc32.rmeta: associated-token-account/src/lib.rs associated-token-account/src/tools.rs
+
+associated-token-account/src/lib.rs:
+associated-token-account/src/tools.rs:
---
+++ target/debug/deps/apl_token_metadata-7b112e6706b28397.d
@@ -0,0 +1,10 @@
+/home/runner/work/arch-network/arch-network/sdk-repo/target/debug/deps/apl_token_metadata-7b112e6706b28397.d: token-metadata/src/lib.rs token-metadata/src/error.rs token-metadata/src/instruction.rs token-metadata/src/processor.rs token-metadata/src/state.rs token-metadata/src/entrypoint.rs
+
+/home/runner/work/arch-network/arch-network/sdk-repo/target/debug/deps/libapl_token_metadata-7b112e6706b28397.rmeta: token-metadata/src/lib.rs token-metadata/src/error.rs token-metadata/src/instruction.rs token-metadata/src/processor.rs token-metadata/src/state.rs token-metadata/src/entrypoint.rs
+
+token-metadata/src/lib.rs:
+token-metadata/src/error.rs:
+token-metadata/src/instruction.rs:
+token-metadata/src/processor.rs:
+token-metadata/src/state.rs:
+token-metadata/src/entrypoint.rs:
