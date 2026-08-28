27c804a9cfe8b9e68008673a67533547ee12399c
GitHub Action <action@github.com>
2026-08-28 10:15:04 +0000

    Sync SDK, Program, Token, Associated Token Account, and Token Metadata from main repository

---
+++ target/debug/deps/apl_token_metadata-b4a9d650654767f1.d
@@ -0,0 +1,10 @@
+/home/runner/work/arch-network/arch-network/sdk-repo/target/debug/deps/apl_token_metadata-b4a9d650654767f1.d: token-metadata/src/lib.rs token-metadata/src/error.rs token-metadata/src/instruction.rs token-metadata/src/processor.rs token-metadata/src/state.rs token-metadata/src/entrypoint.rs
+
+/home/runner/work/arch-network/arch-network/sdk-repo/target/debug/deps/libapl_token_metadata-b4a9d650654767f1.rmeta: token-metadata/src/lib.rs token-metadata/src/error.rs token-metadata/src/instruction.rs token-metadata/src/processor.rs token-metadata/src/state.rs token-metadata/src/entrypoint.rs
+
+token-metadata/src/lib.rs:
+token-metadata/src/error.rs:
+token-metadata/src/instruction.rs:
+token-metadata/src/processor.rs:
+token-metadata/src/state.rs:
+token-metadata/src/entrypoint.rs:
